cdc4ad1c282aae9960a004fe19e6d7b8eb00150c
Stuart Mahon <stuart.mahon@alaress.com.au>
2017-12-26 17:44:28 +1100

    Fixes / Project Part 2

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,17 +2,16 @@ import React from 'react';
 import './SearchBar.css';
 
 const sortByOptions = {
-    'Best Match': best_match,
-    'Highest Rated': rating,
-    'Most Reviewed': review_count
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
 };
 
 class SearchBar extends React.Component {
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
-            // TODO this might need to sit outside the return
-            return <li key="{sortByOptionValue}">sortByOption</li>;
+            return <li key={sortByOptionValue}>sortByOption</li>;
         });
     }
 
@@ -21,7 +20,7 @@ class SearchBar extends React.Component {
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
-                        renderSortByOptions();
+                        {this.renderSortByOptions()};
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
